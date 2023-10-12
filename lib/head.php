b5d1da2388e200a59756d5fb5d00e22e5486c691
Manuel Francis Correya <manufcorreya@gmail.com>
2023-10-12 13:02:10 +0530

    paypal front-end button - completed

--- lib/head.php
+++ lib/head.php
@@ -8,9 +8,33 @@ function rabbit_integrator_enqueue_front_script_style(){
 }
 add_action('wp_enqueue_scripts', 'rabbit_integrator_enqueue_front_script_style');
 function rabbit_integrator_shortcode( $atts ) {
+    global $wpdb;
     $rabbit_integrator_atts = shortcode_atts( array(
         'id' => '',
     ), $atts );
+
+    $id = $rabbit_integrator_atts['id'];
+
+    $title = '';
+    $price = '';
+    $button_text = 'Continue With';
+    $button_text_size = '';
+    $button_width = '';
+    $button_height = '';
+    $button_text_color = '';
+    $button_color = '';
+    $templateResults = $wpdb->get_results("SELECT * FROM " . $wpdb->prefix . "rabbit_integrator_template WHERE template_id = '$id'", ARRAY_A);
+    if($templateResults)
+    {
+        $title = $templateResults[0]['template_title'];
+        $price = empty($templateResults[0]['template_price']) ? '' : $templateResults[0]['template_price'];    
+        $button_text = $templateResults[0]['template_btn_txt']; 
+        $button_text_size = $templateResults[0]['template_btn_txt_size'];
+        $button_width = $templateResults[0]['template_btn_width'];
+        $button_height = $templateResults[0]['template_btn_height'];
+        $button_text_color = $templateResults[0]['template_btn_txt_color'];
+        $button_color = $templateResults[0]['template_btn_bg_color'];
+    }
 
     ob_start(); // Start output buffering
     include('class.paypal.php');
@@ -29,9 +53,20 @@ function rabbit_integrator_shortcode( $atts ) {
         echo $paypal_html = $paypal->submit_paypal_post('full'); 
     }
     ?>
+    <style>
+        .rabbit-integrator-paypal-btn {
+        <?php
+            echo empty($button_text_size) ? '' : 'font-size:'.$button_text_size.'px;';
+            echo empty($button_width) ? '' : 'width:'.$button_width.'px;';
+            echo empty($button_height) ? '' : 'height:'.$button_height.'px;';
+            echo empty($button_text_color) ? '' : 'color:'.$button_text_color.';';
+            echo empty($button_color) ? '' : 'background-color:'.$button_color.';';
+        ?>
+        }
+    </style>
     <div class="rabbit-integrator-paypal-btn-wrap">
         <form method="post" id="rabbit-integrator-submit-form">
-            <button class="rabbit-integrator-paypal-btn" type="submit" name="rabbit-integrator-submit-btn"><span>Continue With</span><img src="<?php echo RI_PLUGIN_URL; ?>assets/images/paypal.png"></button>
+            <button class="rabbit-integrator-paypal-btn" type="submit" name="rabbit-integrator-submit-btn"><span><?php echo $button_text; ?></span><img src="<?php echo RI_PLUGIN_URL; ?>assets/images/paypal.png"></button>
         </form>
         <?php
             if($rabbit_integrator_atts['id'] == '') {
